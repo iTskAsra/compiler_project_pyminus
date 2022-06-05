3105af3444ac3af47d7b7f0b6e4df7694ac3f30e
Sara <sara@carriot.ir>
2022-06-05 19:17:25 +0430

    update CodeGenerator.py

--- CodeGenerator.py
+++ CodeGenerator.py
@@ -5,6 +5,11 @@ from stack import Stack
 
 class CodeGenerator:
     def __init__(self):
+        self.current_id = None
+        self.current_keyword = None
+        self.current_number = None
+        self.current_function = None
+        self.current_symbol = None
         self.symbol_stack = []
         self.stack = Stack()
         self.memory_address = []
@@ -21,6 +26,13 @@ class CodeGenerator:
         address = self.find_address(token)
         self.stack.push(address)
 
+    def assign_func(self):
+        self.generate_formatted_code(
+            'ASSIGN', self.semantic_stack[-1], self.semantic_stack[-2], '')
+
+        num1_type = self.get_type(self.semantic_stack.pop())
+        num2_type = self.get_type(self.semantic_stack[-1])
+
     def calculation(self):
         sign = 'SUB'
         symbol_element = self.symbol_stack.pop()
@@ -36,11 +48,15 @@ class CodeGenerator:
         num1_type, num2_type = self.get_type(num1), self.get_type(num2)
         self.semantic_stack.append(temp)
 
+    def push_number_dec(self):
+        self.semantic_stack.append(self.current_num)
     def generate_formatted_code(self, relop: str, s1, s2, s3):
         self.program_block.append(f'({relop}, {s1}, {s2}, {s3})')
 
-    def insert_formatted_code(self, index: int, relop: str, string1, string2, string3):
-        self.program_block[index] = f'({relop}, {string1}, {string2}, {string3})'
+    def insert_formatted_code(self, index: int,
+                              relop: str, string1, string2, string3):
+        self.program_block[index] = \
+            f'({relop}, {string1}, {string2}, {string3})'
 
     def find_address(self, token):
         return 0  # the token address
