036bb2311b4fc41787d47ee060d10652791e861b
Kasra <kasra138079@gmail.com>
2022-06-13 13:27:26 +0430

    correct code gen.

--- code_generator.py
+++ code_generator.py
@@ -1,5 +1,5 @@
 from stack import Stack
-from three_address_code import *
+from three_address_code import ThreeAddressCode
 
 
 class CodeGenerator:
@@ -20,94 +20,136 @@ class CodeGenerator:
         self.pb_pointer = 0
 
         self.semantic_routines = {
-            'pid': pid_func,
-            'pnum': push_number,
-            'label': label
+            'pid': pid,
+            'pnum': pnum,
+            'label': label,
+            'relop_sign': relop_sign,
+            'jpf_save': jpf_save,
+            'save': save,
+            'jp': jp,
+            'jpf': jpf,
+            'while': while_func,
+            'add': add,
+            'sub': sub,
+            'mult': mult,
+            'power': power,
+            'assign': assign,
+            'relop': relop
         }
 
     def call_routine(self, routine, token):
-        self.semantic_routines[routine](token)
+        self.semantic_routines[routine]()
 
-    def get_temp_func(self) -> int:
-        return self.temp_block.get_first_empty_cell()
+    def add_code_to_pb(self, operation, lhs, rhs, target):
+        self.program_block.append(ThreeAddressCode(self.pb_ptr, operation, lhs, rhs, target))
+        self.pb_ptr += 1
 
-    def pid_func(self):
+    def find_token_address(self, token):
+        token_address = token.address
+        return token_address  # return token address, not implemented!
+
+    def pid(self):
         address = self.find_address(self.current_id)
-        self.stack.push(address)
+        self.semantic_stack.push(address)
 
-    def assign_func(self):
-        self.generate_formatted_code(
-            'ASSIGN', self.semantic_stack[-1], self.semantic_stack[-2], '')
-        num1_type = self.get_type(self.semantic_stack.pop())
-        num2_type = self.get_type(self.semantic_stack[-1])
+    def pnum(self):
+        self.semantic_stack.push(self.current_number)
 
-    def calculation(self):
-        sign = 'SUB'
-        symbol_element = self.symbol_stack.pop()
-        if symbol_element == '+':
-            sign = 'ADD'
-        elif symbol_element == '*':
-            sign = 'MULT'
-        temp = self.get_temp_func()
-        self.generate_formatted_code(
-            sign,
-            self.semantic_stack[-2],
-            self.semantic_stack[-1],
-            temp
-        )
-        num1 = self.stack.pop()
-        num2 = self.stack.pop()
-        num1_type, num2_type = self.get_type(num1), self.get_type(num2)
-        self.semantic_stack.append(temp)
+    def relop_sign(self):
+        self.semantic_stack.push(self.current_symbol)  ######################### to check
 
-    def push_number_dec(self):
-        self.semantic_stack.append(self.current_num)
+    def reserve_pb(self):
+        self.program_block.append('')
 
-    def push_number(self):
-        self.semantic_stack.append(f'#{self.current_number}')
+    def label(self):
+        line = len(self.program_block)
+        self.semantic_stack.push(line)
+        self.reserve_pb()
 
+    def save(self):
+        line = len(self.program_block)
+        self.semantic_stack.push(line)
 
-
-    def insert_formatted_code(self, index: int, relop: str,
-                              string1, string2, string3):
-        self.program_block[index] = \
-            f'({relop}, {string1}, {string2}, {string3})'
-
-    def find_address(self, token):
-        return 0  # the token address
-
-    def get_type(self, var):
-        pass
-
-    def jp_func(self):
-
-        jp_point = self.semantic_stack.pop()
-        tac = ThreeAddressCode(self.pb_pointer, 'JP', jp_point)
-        self.program_block.append(tac)
-
-    def save_function(self):
-        self.semantic_stack.append(len(self.program_block))
-        self.program_block.append("")
-
-    def jpf_func(self):
-        self.insert_formatted_code(
-            self.semantic_stack[-1],
-            "JPF",
-            self.semantic_stack[-2],
-            len(self.program_block), ""
-        )
-
-    def save_jpf_func(self):
-        self.insert_formatted_code(
-            self.semantic_stack[-1],
-            'JPF',
-            self.semantic_stack[-2],
-            len(self.program_block) + 1,
-            ''
-        )
+    def jpf_save(self):
+        i = len(self.program_block)
+        self.add_code_to_pb("JPF", self.semantic_stack.peek(-1), i, '')
         self.semantic_stack.pop()
         self.semantic_stack.pop()
-        self.save_function()
+
+    def jp(self):
+        i = len(self.program_block)
+        self.add_code_to_pb("JP", i, '', '')
+        self.semantic_stack.pop()
+
+    def jpf(self):
+        i = len(self.program_block)  # ???????compare to jpf_save
+        self.add_code_to_pb("JPF", self.semantic_stack.peek(-1), i, '')
+        self.semantic_stack.pop()
+        self.semantic_stack.pop()
+
+    def while_func(self):  #####is it true? idk
+        i = len(self.program_block)
+        self.add_code_to_pb("JPF", self.semantic_stack.peek(-1), i, '')  # i+1
+        self.program_block.insert(i, three_addr_code("JP", self.semantic_stack.peek(-2), '', ''))
+        i += 1
+        self.semantic_stack.pop()
+        self.semantic_stack.pop()
+        self.semantic_stack.pop()
+
+    def add(self):
+        num1 = self.semantic_stack.peek(0)
+        num2 = self.semantic_stack.peek(-1)
+        t = num1 + num2
+        self.add_code_to_pb("ADD", num1, num2, t)
+
+    def sub(self):
+        t = get_temp()
+        num1 = self.semantic_stack.peek(0)
+        num2 = self.semantic_stack.peek(-1)
+        t = num1 - num2
+        self.add_code_to_pb("SUB", num1, num2, t)
+
+    def mult(self):
+        num1 = self.semantic_stack.peek(0)
+        num2 = self.semantic_stack.peek(-1)
+        t = num1 * num2
+        self.add_code_to_pb("MULT", num1, num2, t)
+
+    def power(self):
+        num1 = self.semantic_stack.peek(0)  # num2 ^ num1
+        num2 = self.semantic_stack.peek(-1)
+        i = num1
+        while i > 0:
+            self.semantic_stack.push(num2)
+            self.semantic_stack.push(num2)
+            self.mult()
+            i -= 1
+
+    def assign(self):
+        self.add_code_to_pb("ASSIGN", self.semantic_stack.peek(0), self.semantic_stack.peek(-1), '')
+
+    def relop(self):
+        # Relational_Expression⟶Expression Relop Expression #relop
+        num1 = self.semantic_stack.pop()
+        relop = self.semantic_stack.pop()  # how to find relop????????????????????????????????????????????????
+        num2 = self.semantic_stack.pop()
+        if relop == '<':
+            if num1 < num2:
+                r = 1
+            else:
+                r = 0
+            self.add_code_to_pb('LT', num1, num2, r)
+        elif relop == '==':
+            if num1 == num2:
+                r = 1
+            else:
+                r = 0
+            self.add_code_to_pb('EQ', num1, num2, r)
+
+    def return_func(self):
+        return_value = self.semantic_stack.pop()
+
+    ##########################
 
     def check_temp_address(self, address):
         return address >= self.temp_block.starting_index
